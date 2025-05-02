600d468464c17891cdecab1b3ea25865c4e8a917
Deepak-patel-here <deepak.patel.23082005@gmail.com>
2025-05-02 15:35:58 +0530

    hello api

--- app/src/main/java/com/deepakjetpackcompose/ainotes/model/client/ApiClient.kt
+++ app/src/main/java/com/deepakjetpackcompose/ainotes/model/client/ApiClient.kt
@@ -13,7 +13,7 @@ import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.json.Json
 
 object ApiClient{
-    private val apiKey="AIzaSyCKrjUbuuVcdJlRCK1yq1bAQUoV47NVgPE"
+    private val apiKey=""
     val client: HttpClient= HttpClient{
         install(ContentNegotiation){
             json(json = Json{ignoreUnknownKeys=true})
